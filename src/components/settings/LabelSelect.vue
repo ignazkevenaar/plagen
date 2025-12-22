baedf060a7caae274d1d7313f22a2f4438043404
Ignaz Kevenaar <ignazkevenaar@gmail.com>
2025-12-22 19:27:35 +0100

    Add arrow to selects

--- src/components/settings/LabelSelect.vue
+++ src/components/settings/LabelSelect.vue
@@ -17,19 +17,45 @@ const props = defineProps({
     <span class="mb-1 block text-[10pt] font-normal opacity-60 select-none">
       {{ sublabel }}
     </span>
-    <select
-      v-model="model"
-      v-bind="$attrs"
-      class="w-full appearance-none rounded-md bg-gray-100 p-1 dark:bg-gray-900"
-      :class="large ? 'text-3xl font-normal' : ''"
-    >
-      <option
-        v-for="option in options"
-        :value="option.value ?? option"
-        :key="option.value ?? option"
+    <div class="selectContainer">
+      <select
+        v-model="model"
+        v-bind="$attrs"
+        class="w-full appearance-none rounded-md bg-gray-100 p-1 dark:bg-gray-900"
+        :class="large ? 'text-3xl font-normal' : ''"
       >
-        {{ option.text ?? option }}
-      </option>
-    </select>
+        <option
+          v-for="option in options"
+          :value="option.value ?? option"
+          :key="option.value ?? option"
+        >
+          {{ option.text ?? option }}
+        </option>
+      </select>
+    </div>
   </Label>
 </template>
+
+<style scoped>
+.selectContainer {
+  position: relative;
+  width: 100%;
+
+  &::after {
+    display: block;
+    position: absolute;
+    top: 12px;
+    inset-inline-end: 6px;
+    border-top: 8px solid currentColor;
+    border-right: 4px solid transparent;
+    border-left: 4px solid transparent;
+    width: 0;
+    height: 0;
+    content: "";
+  }
+
+  &:has(select:open)::after {
+    transform: scale(1, -1);
+  }
+}
+</style>
