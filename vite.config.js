4d73a2c7680133776f1210d7e6fb0c5b561c04f0
Ignaz Kevenaar <ignazkevenaar@gmail.com>
2026-06-13 11:37:56 +0200

    Replaced downloaded fonts with custom downloader to fix incorrect height

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
-import webfontDownload from "vite-plugin-webfont-dl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), webfontDownload()],
+  plugins: [vue(), tailwindcss()],
   base: "/plagen/",
 });
